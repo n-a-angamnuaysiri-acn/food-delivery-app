7b096b8b9524135b7a36b061922e9659dd9e1b7d
n.a.angamnuaysiri <n.a.angamnuaysiri@accenture.com>
2024-11-18 17:16:59 +0700

    feat: Improve main function

--- main.go
+++ main.go
@@ -6,14 +6,30 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
+	"github.com/n-a-angamnuaysiri-acn/food-delivery-app/config"
+	"github.com/n-a-angamnuaysiri-acn/food-delivery-app/restaurant"
 )
 
 func main() {
 	app := echo.New()
 	app.Use(middleware.Recover())
+	app.Use(middleware.Logger())
+
 	app.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+	app.GET("/restaurant", restaurant.GetRestaurants)
+
+	// Connect To Database
+	config.DatabaseInit()
+	gorm := config.Database()
+
+	dbGorm, err := gorm.DB()
+	if err != nil {
+		panic(err)
+	}
+
+	dbGorm.Ping()
 
 	log.Fatal(app.Start(":8080"))
 }
