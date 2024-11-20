55ed16ba2ca4175434c8c94f97a03ea4e3c7a69c
n.a.angamnuaysiri <n.a.angamnuaysiri@accenture.com>
2024-11-20 14:14:16 +0700

    feat: start implementing customer service

---
+++ customer-service/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
+)
+
+func main() {
+	app := echo.New()
+	app.Use(middleware.Recover())
+	app.Use(middleware.Logger())
+
+	app.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	log.Fatal(app.Start(":8086"))
+}
