eb21ae0f7c80b47cd199d63e8adae3dc79fc7095
n.a.angamnuaysiri <n.a.angamnuaysiri@accenture.com>
2024-11-21 10:10:14 +0700

    feat: implement database config to customer service

---
+++ customer-service/config/config.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/gommon/log"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+)
+
+var database *gorm.DB
+
+func DatabaseInit() {
+	var err error
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta", host, user, password, dbName, port)
+	database, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			TablePrefix:   "app_data.", // schema name
+			SingularTable: false,
+		}})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Info("Successfully connected to PostgreSQL database")
+}
+
+func Database() *gorm.DB {
+	return database
+}
