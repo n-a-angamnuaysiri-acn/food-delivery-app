9c550bf442c808fcfbe7347a025468055d6a0c19
n.a.angamnuaysiri <n.a.angamnuaysiri@accenture.com>
2024-11-21 10:47:23 +0700

    feat: implement order related models

---
+++ customer-service/model/model.go
@@ -0,0 +1,23 @@
+package model
+
+type Order struct {
+	Id           uint   `gorm:"primaryKey" json:"id"`
+	RestaurantId string `json:"restaurant_id"`
+	Items        string `gorm:"default:[]"`
+	Status       string `json:"status"`
+}
+
+type PlaceOrderResquest struct {
+	RestaurantId string `json:"restaurant_id"`
+	Items        []Item `json:"items"`
+}
+
+type OrderStatusResponse struct {
+	OrderId string `json:"order_id"`
+	Status  string `json:"status"`
+}
+
+type Item struct {
+	MenuId   string `json:"menu_id"`
+	Quantity uint   `json:"quantity"`
+}
