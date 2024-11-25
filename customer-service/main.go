7985598244560b37c9505927663963cf6613fbac
n.a.angamnuaysiri <n.a.angamnuaysiri@accenture.com>
2024-11-25 12:30:10 +0700

    feat: improve main functions to handle exit gracefully

--- customer-service/main.go
+++ customer-service/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"customer-service/config"
 	"customer-service/service"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -11,6 +15,22 @@ import (
 )
 
 func main() {
+	// Connect To Database
+	go func() {
+		config.DatabaseInit()
+		gorm := config.Database()
+
+		dbGorm, err := gorm.DB()
+		if err != nil {
+			panic(err)
+		}
+		err = dbGorm.Ping()
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	// Set Up Apis
 	app := echo.New()
 	app.Use(middleware.Recover())
 	app.Use(middleware.Logger())
@@ -20,16 +40,21 @@ func main() {
 	})
 	app.POST("/order", service.PlaceOrder)
 
-	// Connect To Database
-	config.DatabaseInit()
-	gorm := config.Database()
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
 
-	dbGorm, err := gorm.DB()
-	if err != nil {
-		panic(err)
+	// Start server
+	go func() {
+		if err := app.Start(":8086"); err != nil && err != http.ErrServerClosed {
+			log.Fatal("shutting down the server")
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shut down the server with a timeout of 10 seconds.
+	<-ctx.Done()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := app.Shutdown(ctx); err != nil {
+		log.Fatal(err)
 	}
-
-	dbGorm.Ping()
-
-	log.Fatal(app.Start(":8086"))
 }
--- rider-service/main.go
+++ rider-service/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"context"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -11,6 +15,21 @@ import (
 )
 
 func main() {
+	// Connect To Database
+	go func() {
+		config.DatabaseInit()
+		gorm := config.Database()
+
+		dbGorm, err := gorm.DB()
+		if err != nil {
+			panic(err)
+		}
+		err = dbGorm.Ping()
+		if err != nil {
+			panic(err)
+		}
+	}()
+
 	app := echo.New()
 	app.Use(middleware.Recover())
 	app.Use(middleware.Logger())
@@ -22,16 +41,21 @@ func main() {
 	app.POST("/rider/order/pickup", service.PickUpOrder)
 	app.POST("/rider/order/deliver", service.DeliverOrder)
 
-	// Connect To Database
-	config.DatabaseInit()
-	gorm := config.Database()
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
 
-	dbGorm, err := gorm.DB()
-	if err != nil {
-		panic(err)
+	// Start server
+	go func() {
+		if err := app.Start(":8084"); err != nil && err != http.ErrServerClosed {
+			log.Fatal("shutting down the server")
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shut down the server with a timeout of 10 seconds.
+	<-ctx.Done()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := app.Shutdown(ctx); err != nil {
+		log.Fatal(err)
 	}
-
-	dbGorm.Ping()
-
-	log.Fatal(app.Start(":8084"))
 }
