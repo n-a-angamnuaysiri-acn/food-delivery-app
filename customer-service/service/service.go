64063d4c4f5d5a1b90ed25c04aa67295850dd604
n.a.angamnuaysiri <n.a.angamnuaysiri@accenture.com>
2024-11-26 13:18:35 +0700

    feat: implement place order notification logic

--- customer-service/service/service.go
+++ customer-service/service/service.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"customer-service/client"
 	"customer-service/model"
 	"customer-service/repository"
 
@@ -26,5 +27,27 @@ func PlaceOrder(ctx echo.Context) error {
 		}
 		return ctx.JSON(http.StatusOK, data)
 	}
-	return ctx.JSON(http.StatusOK, model.OrderStatusResponse{OrderId: fmt.Sprintf("%v", order.Id), Status: order.Status})
+	restaurant, err := repository.FindRestaurantById(request.RestaurantId)
+	if err != nil {
+		log.Error(err)
+		data := map[string]interface{}{
+			"message": err.Error(),
+		}
+		return ctx.JSON(http.StatusOK, data)
+	}
+	orderId := fmt.Sprintf("%d", order.Id)
+	notificationRequest := model.NotificationRequest{
+		Recipient: "restaurant",
+		OrderId: orderId,
+		Message: fmt.Sprintf("Order for restaurant %s is created with menus %v.", restaurant.Name, order.Items),
+	}
+	err = client.SendNotification(notificationRequest)
+	if err != nil {
+		log.Error(err)
+		data := map[string]interface{}{
+			"message": err.Error(),
+		}
+		return ctx.JSON(http.StatusOK, data)
+	}
+	return ctx.JSON(http.StatusOK, model.OrderStatusResponse{OrderId: orderId, Status: order.Status})
 }
