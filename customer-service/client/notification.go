9da0697405e7f93d38ddba110c5a558d9ecd99f5
n.a.angamnuaysiri <n.a.angamnuaysiri@accenture.com>
2024-11-26 13:17:33 +0700

    feat: add notification client to customer service

---
+++ customer-service/client/notification.go
@@ -0,0 +1,42 @@
+package client
+
+import (
+	"bytes"
+	"customer-service/config"
+	"customer-service/model"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/labstack/gommon/log"
+)
+
+func SendNotification(request model.NotificationRequest) error {
+	log.Info(fmt.Sprintf("Sending Request /notification/send: %v", request))
+	jsonBody, err := json.Marshal(request)
+	if err != nil {
+		return err
+	}
+	response, err := http.Post(config.NotificationSendUrl, "application/json", bytes.NewBuffer(jsonBody))
+	if err != nil {
+		return err
+	}
+	if response.StatusCode != http.StatusOK {
+		message := fmt.Sprintf("Non-OK HTTP status: %d\n", response.StatusCode)
+		return errors.New(message)
+	}
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+	var result model.NotificationResponse
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		return err
+	}
+	log.Info(fmt.Sprintf("Receive Response /notification/send: %v", result))
+	return nil
+}
---
+++ customer-service/config/client.go
@@ -0,0 +1,3 @@
+package config
+
+const NotificationSendUrl = "http://localhost:8088/notification/send"
